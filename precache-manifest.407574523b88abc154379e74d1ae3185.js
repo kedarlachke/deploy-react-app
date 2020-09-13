2f1cd28eadb4acab1160098b635d556c8dcc301c
Kedar Lachke <kedarlachke1@gmail.com>
2020-09-13 21:47:45 +0800

    Updates

---
+++ precache-manifest.407574523b88abc154379e74d1ae3185.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edaae25f139eba096632cf98005ebfbb",
+    "url": "/deploy_project/index.html"
+  },
+  {
+    "revision": "69bf0bfda8c2479efbce",
+    "url": "/deploy_project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "875796afc931ea609961",
+    "url": "/deploy_project/static/js/2.c8c5862e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy_project/static/js/2.c8c5862e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69bf0bfda8c2479efbce",
+    "url": "/deploy_project/static/js/main.4ef66f5f.chunk.js"
+  },
+  {
+    "revision": "94840df4aa8ce70e142c",
+    "url": "/deploy_project/static/js/runtime-main.c6851160.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy_project/static/media/logo.5d5d9eef.svg"
+  }
+]);
